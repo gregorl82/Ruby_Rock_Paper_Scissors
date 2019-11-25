11dd10ffecbe32081fe446c04cc392e5d496c7a8
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-25 16:57:45 +0000

    Refactors Game class to include downcase method

--- models/game.rb
+++ models/game.rb
@@ -1,18 +1,20 @@
 class Game
 
   def self.check(player1, player2)
+    player1.downcase!
+    player2.downcase!
     case
-      when player1 == "Rock" && player2 == "Scissors"
+    when player1 == "rock" && player2 == "scissors"
         return "Rock"
-      when player1 == "Rock" && player2 == "Paper"
+      when player1 == "rock" && player2 == "paper"
         return "Paper"
-      when player1 == "Scissors" && player2 == "Rock"
+      when player1 == "scissors" && player2 == "rock"
         return "Rock"
-      when player1 == "Scissors" && player2 == "Paper"
+      when player1 == "scissors" && player2 == "paper"
         return "Scissors"
-      when player1 == "Paper" && player2 == "Rock"
+      when player1 == "paper" && player2 == "rock"
         return "Paper"
-      when player1 == "Paper" && player2 == "Scissors"
+      when player1 == "paper" && player2 == "scissors"
         return "Scissors"
       when player1 == player2
         return "No one"
