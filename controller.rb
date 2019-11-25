b8a5d9e0a0c8c0429eb1a941a459f9b833f5c32a
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-25 16:52:29 +0000

    Adds routes to controller.rb and result view

---
+++ controller.rb
@@ -0,0 +1,12 @@
+require('sinatra')
+require('sinatra/contrib/all') if development?
+
+require_relative('./models/game.rb')
+also_reload('./models/*')
+
+get '/:player1/:player2' do
+  player1 = params['player1']
+  player2 = params['player2']
+  @result = Game.check(player1, player2)
+  erb(:result)
+end
