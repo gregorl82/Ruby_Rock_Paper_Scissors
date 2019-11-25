146d018f1bcd7241c88f6ea4f730372875eb807b
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-25 18:06:13 +0000

    Adds CSS styling to html

--- controller.rb
+++ controller.rb
@@ -4,6 +4,10 @@ require('sinatra/contrib/all') if development?
 require_relative('./models/game.rb')
 also_reload('./models/*')
 
+get '/' do
+  erb(:welcome)
+end
+
 get '/:player1/:player2' do
   player1 = params['player1']
   player2 = params['player2']
